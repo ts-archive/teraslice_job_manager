fd55787354ad26d15c96bf6c3ff6c5fbce0181ed
Charlie Iorg <ciorg@terascope.io>
2018-07-02 11:25:39 -0600

    fixed restart cmd

--- cmds/restart.js
+++ cmds/restart.js
@@ -2,20 +2,19 @@
 
 const _ = require('lodash');
 
-exports.command = 'restart <jobFile>';
+exports.command = 'restart <job_file>';
 exports.desc = 'stops and starts a job\n';
 exports.builder = (yargs) => {
     yargs.example('tjm restart jobfile.prod.json');
 };
 exports.handler = (argv, _testFunctions) => {
     const reply = require('./cmd_functions/reply')();
-    const jsonData = require('./cmd_functions/json_data_functions')();
-    const jobContents = jsonData.jobFileHandler(argv.jobFile)[1];
-    jsonData.metaDataCheck(jobContents);
-    const tjmFunctions = _testFunctions || require('./cmd_functions/functions')(argv, jobContents.tjm.cluster);
+    require('./cmd_functions/json_data_functions')(argv).returnJobData();
+    const tjmFunctions = _testFunctions || require('./cmd_functions/functions')(argv);
+    const jobContents = argv.job_file_content;
     const jobId = jobContents.tjm.job_id;
 
-    return tjmFunctions.alreadyRegisteredCheck(jobContents)
+    return tjmFunctions.alreadyRegisteredCheck()
         .then(() => tjmFunctions.teraslice.jobs.wrap(jobId).stop())
         .then((stopResponse) => {
             if (!stopResponse.status.status === 'stopped') {
